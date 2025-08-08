99ee418bd972b931025202813a3cdd999e86359d
oussama <oussama@gmail.com>
2025-08-08 18:49:53 +0100

    add another reducers (increase and decrease)

--- food-delivery/src/features/cart/cartSlice.js
+++ food-delivery/src/features/cart/cartSlice.js
@@ -2,44 +2,50 @@ import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 const initialState = {
-  cart: [],
+   cartItems: [],
+
 };
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: " cartItems",
   initialState,
 
   reducers: {
     addToCart(state, action) {
-      const existingItem = state.cart.find(
+      const existingItem = state. cartItems.find(
         (item) => item.id === action.payload.id
       );
       if (existingItem) {
         existingItem.quantity += 1;
         toast.info(action.payload.name + '  quantity has been increased', { autoClose: 1000, className: '  w-2/3 md:w-full m-1' });
       } else {
-        state.cart.push({ ...action.payload, quantity: 1 });
+        state.cartItems.push({ ...action.payload, quantity: 1 });
         toast.success(action.payload.name + ' added to cart', { autoClose: 1000, className: 'text-sm   w-2/3 md:w-full m-1' });
       }
     },
-    increaseQuantity(state , action) {
-        const item = state.cart.find((item) => item.id === action.payload.id);
-        if(item){
-          item.quantity+=1
-        }
+    increaseQuantity(state, action) {
+      const item = state.cartItems.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity += 1
+      }
     },
-    decreaseQuantity(state , action) {
-        const item = state.cart.find((item) => item.id === action.payload.id);
-        if(item && item.quantity > 1){
-          item.quantity-=1
-        }
+    decreaseQuantity(state, action) {
+      const item = state.cartItems.find((item) => item.id === action.payload.id);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1
+      }
     },
     removeFromCart(state, action) {
-      state.cart = state.cart.filter((item) => item.id !== action.payload.id)
-
-    }
+      state.cartItems = state.cartItems.filter((item) => item.id !== action.payload.id)
+    },
+  
   },
 });
 
-export const { addToCart, removeFromCart , decreaseQuantity , increaseQuantity } = cartSlice.actions;
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectIsAdded = (productId) => (state) =>
+  state.cart.cartItems.some((item) => item.id === productId);
+
+
+export const { addToCart, removeFromCart, decreaseQuantity, increaseQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
